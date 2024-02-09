7a126bbc95b9489722de5fceac7d3e5ba9148513
Haripriya <arrahppk@gmail.com>
2024-02-10 01:15:01 +0530

    navbar changes

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('scroll', function() {
+  var navbar = document.getElementById('navbar');
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > 50) {
+    navbar.style.backdropFilter = 'blur(3px)';
+      navbar.style.backgroundColor = 'rgba(50, 50, 50, 0.8)';
+      
+  } else {
+    navbar.style.backdropFilter = 'none';
+      navbar.style.backgroundColor = 'transparent';
+  }
+});
+
+
